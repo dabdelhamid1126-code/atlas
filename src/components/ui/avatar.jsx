a2d2883e5e6993b7f74af432f1dab0b7b8d3c7f2
vixoustm <vixoustm@gmail.com>
2026-01-14 05:11:37 +0000

    File changes

--- src/components/ui/avatar.jsx
+++ src/components/ui/avatar.jsx
@@ -1,28 +1,30 @@
+
 "use client"
 
 import * as React from "react"
-import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
 
 const Avatar = React.forwardRef(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Root
+  <div
     ref={ref}
     className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
     {...props} />
 ))
-Avatar.displayName = AvatarPrimitive.Root.displayName
+Avatar.displayName = "Avatar"
 
-const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image
+const AvatarImage = React.forwardRef(({ className, src, alt, ...props }, ref) => (
+  <img
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    src={src}
+    alt={alt}
+    className={cn("aspect-square h-full w-full object-cover", className)}
     {...props} />
 ))
-AvatarImage.displayName = AvatarPrimitive.Image.displayName
+AvatarImage.displayName = "AvatarImage"
 
 const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Fallback
+  <div
     ref={ref}
     className={cn(
       "flex h-full w-full items-center justify-center rounded-full bg-muted",
@@ -30,6 +32,6 @@ const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
     )}
     {...props} />
 ))
-AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+AvatarFallback.displayName = "AvatarFallback"
 
 export { Avatar, AvatarImage, AvatarFallback }
