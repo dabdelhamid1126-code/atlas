8e1484909ed7fce82c2e42cd78e906f5d6d57360
Dalia distro <daliadistrollc@gmail.com>
2026-03-12 18:56:18 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Settings from './pages/Settings';
 import Goals from './pages/Goals';
+import Transactions from './pages/Transactions';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
