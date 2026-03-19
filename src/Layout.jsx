a6879d3d6786a8d876a297be2033122df762f82b
Dalia distro <daliadistrollc@gmail.com>
2026-03-19 17:02:42 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -17,7 +17,8 @@ import {
   LogOut,
   ChevronRight,
   Settings as SettingsIcon,
-  ChevronDown
+  ChevronDown,
+  Plus
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
