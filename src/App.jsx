4c4262e169954d297ee227ef9565ea4bf7124e7d
dabdelhamid1126-code <dabdelhamid1126@gmail.com>
2026-05-07 01:42:38 -0400

    Import ProtectedRoute component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ProtectedRoute } from './components/ProtectedRoute';
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
